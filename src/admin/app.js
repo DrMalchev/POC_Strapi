1895bb0633660260ad9c85a39d6a20a7ffedee07
DrMalchev <dr.delyan.malchev@gmail.com>
2023-10-26 15:46:21 +0300

    Adding translation for the main part - no plugins

---
+++ src/admin/app.js
@@ -0,0 +1,13 @@
+export default {
+  config: {
+    locales: ['en', 'bg'],
+    languageNativeNames: {
+      en: 'English',
+      bg: 'Български'
+    },
+    translations: {
+      bg: '.src/admin/translations/bg'
+    },
+  },
+  bootstrap() { },
+};
